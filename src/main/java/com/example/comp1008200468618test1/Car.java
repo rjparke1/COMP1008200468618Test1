a2d99dc8c89a17b40ed1fbf8e99dfb9252813dbe
rjpark1 <rjparke1@lakeheadu.ca>
2023-02-01 08:50:32 -0500

    question 2 complete

---
+++ src/main/java/com/example/comp1008200468618test1/Car.java
@@ -0,0 +1,10 @@
+package com.example.comp1008200468618test1;
+
+public class Car {
+    public String make;
+    public String model;
+    public double price;
+    public int year;
+}
+
+
---
+++ src/main/java/com/example/comp1008200468618test1/Dealership.java
@@ -0,0 +1,8 @@
+package com.example.comp1008200468618test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership{
+ ArrayList inventory = new ArrayList<Car>();
+}
